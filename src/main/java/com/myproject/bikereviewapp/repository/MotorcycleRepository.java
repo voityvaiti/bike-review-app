fd98163b3b5f3fff6df1cedba60489f4589abcf6
voityvaiti <flashlight@i.ua>
2023-09-24 19:22:44 +0300

    Spring Data repositories creation

---
+++ src/main/java/com/myproject/bikereviewapp/repository/MotorcycleRepository.java
@@ -0,0 +1,9 @@
+package com.myproject.bikereviewapp.repository;
+
+import com.myproject.bikereviewapp.entity.Motorcycle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MotorcycleRepository extends JpaRepository<Motorcycle, Long> {
+}
