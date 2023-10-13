2aae5a49b53ba69f95622674f67de8745c58ec57
voityvaiti <flashlight@i.ua>
2023-10-13 21:56:05 +0300

    Get Reviews by Motorcycle feature impl in Repository and Service layer

--- src/main/java/com/myproject/bikereviewapp/repository/ReviewRepository.java
+++ src/main/java/com/myproject/bikereviewapp/repository/ReviewRepository.java
@@ -1,9 +1,15 @@
 package com.myproject.bikereviewapp.repository;
 
+import com.myproject.bikereviewapp.entity.Motorcycle;
 import com.myproject.bikereviewapp.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findAllByMotorcycle(Motorcycle motorcycle);
+
 }
--- src/main/java/com/myproject/bikereviewapp/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/myproject/bikereviewapp/service/impl/ReviewServiceImpl.java
@@ -1,19 +1,39 @@
 package com.myproject.bikereviewapp.service.impl;
 
+import com.myproject.bikereviewapp.entity.Motorcycle;
 import com.myproject.bikereviewapp.entity.Review;
+import com.myproject.bikereviewapp.exceptionhandler.exception.EntityNotFoundException;
+import com.myproject.bikereviewapp.repository.MotorcycleRepository;
 import com.myproject.bikereviewapp.repository.ReviewRepository;
 import com.myproject.bikereviewapp.service.abstraction.ReviewService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
 
     private final ReviewRepository reviewRepository;
 
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+    private final MotorcycleRepository motorcycleRepository;
+
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository, MotorcycleRepository motorcycleRepository) {
         this.reviewRepository = reviewRepository;
+        this.motorcycleRepository = motorcycleRepository;
     }
 
+    @Override
+    public List<Review> getReviewsByMotorcycleId(Long id) {
+        Optional<Motorcycle> optionalMotorcycle = motorcycleRepository.findById(id);
+
+        if(optionalMotorcycle.isEmpty()) {
+            throw new EntityNotFoundException("Motorcycle with id" + id + "not found");
+        }
+
+        return reviewRepository.findAllByMotorcycle(optionalMotorcycle.get());
+    }
 
     @Override
     public Review create(Review review) {
