139b086aa6335c2306f21a930c844809cf6aa87b
voityvaiti <flashlight@i.ua>
2023-10-21 19:55:04 +0300

    New method existsByUsername for User service and repo layer. Finishing of create() method

--- src/main/java/com/myproject/bikereviewapp/service/impl/UserServiceImpl.java
+++ src/main/java/com/myproject/bikereviewapp/service/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.myproject.bikereviewapp.service.impl;
 
 import com.myproject.bikereviewapp.entity.User;
+import com.myproject.bikereviewapp.exceptionhandler.exception.EntityNotFoundException;
+import com.myproject.bikereviewapp.exceptionhandler.exception.UserDuplicationException;
 import com.myproject.bikereviewapp.repository.UserRepository;
 import com.myproject.bikereviewapp.service.abstraction.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -16,12 +21,30 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
+    public boolean exists(String username) {
+        return userRepository.existsUserByUsername(username);
+    }
+
+    @Override
     public User findByUsername(String username) {
-        return userRepository.findByUsername(username).orElse(new User());
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+
+        if (optionalUser.isEmpty()) {
+            throw new EntityNotFoundException("User with username " + username + "not found");
+        }
+
+        return optionalUser.get();
     }
 
     @Override
     public User create(User user) {
+
+        if (userRepository.existsUserByUsername(user.getUsername())) {
+            throw new UserDuplicationException("User with username " + user.getUsername() + " already exists");
+        }
+
+        String encodedPassword = new BCryptPasswordEncoder().encode(user.getPassword());
+        user.setPassword(encodedPassword);
 
         return userRepository.save(user);
     }
