10eed06306a034b4c662f9c7a20263a527e9306c
voityvaiti <flashlight@i.ua>
2023-10-31 00:52:09 +0200

    Get all brands (service&controller impl).

--- src/main/java/com/myproject/bikereviewapp/service/abstraction/BrandService.java
+++ src/main/java/com/myproject/bikereviewapp/service/abstraction/BrandService.java
@@ -2,7 +2,11 @@ package com.myproject.bikereviewapp.service.abstraction;
 
 import com.myproject.bikereviewapp.entity.Brand;
 
+import java.util.List;
+
 public interface BrandService {
+
+    List<Brand> getAll();
 
     Brand create(Brand brand);
 
