5cbaf0ddd08b3e0530f94ed97572cf946d290bf9
voityvaiti <flashlight@i.ua>
2023-11-25 00:57:12 +0200

    Added getAll(Pageable) method to BrandService. Removed deprecated getAllSortedByIdAsc()

--- src/main/java/com/myproject/bikereviewapp/repository/BrandRepository.java
+++ src/main/java/com/myproject/bikereviewapp/repository/BrandRepository.java
@@ -4,11 +4,6 @@ import com.myproject.bikereviewapp.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
-
-    List<Brand> findAllByOrderByIdAsc();
-
 }
