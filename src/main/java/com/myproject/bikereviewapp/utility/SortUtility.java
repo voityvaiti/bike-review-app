e826cd8ee095f915393f77155e8a926ec6bc37f9
voityvaiti <flashlight@i.ua>
2023-12-10 18:51:03 +0200

    Refactoring of SortUtility

--- src/main/java/com/myproject/bikereviewapp/utility/SortUtility.java
+++ src/main/java/com/myproject/bikereviewapp/utility/SortUtility.java
@@ -8,7 +8,15 @@ import java.util.regex.Pattern;
 
 public class SortUtility {
 
-    private static final String SORT_FIELD_REGEX = "^\\s*[A-Za-z.]+\\s*(:\\s*((asc|desc)|))?$";
+    private static final String ORDER_EXPRESSION_REGEX = "^\\s*[A-Za-z.]+\\s*(:\\s*((asc|desc)|))?$";
+
+    private static final String SORT_EXPRESSION_DELIMITER = ",";
+
+    private static final String ORDER_EXPRESSION_DELIMITER = ":";
+    private static final int ORDER_EXPRESSION_PROPERTY_INDEX = 0;
+    private static final int ORDER_EXPRESSION_DIRECTION_INDEX = 1;
+
+    private static final Sort DEFAULT_SORT = Sort.by(Sort.Order.asc("id"));
 
     private SortUtility() {
     }
@@ -21,19 +29,19 @@ public class SortUtility {
 
         List<Sort.Order> orders = new ArrayList<>();
 
-        String[] sortFields = sortExpression.split(",");
+        String[] orderExpressions = sortExpression.split(SORT_EXPRESSION_DELIMITER);
 
-        for (String sortField : sortFields) {
-            String[] parts = sortField.split(":");
-            String field = parts[0].trim();
+        for (String orderExpression : orderExpressions) {
+            String[] orderParams = orderExpression.split(ORDER_EXPRESSION_DELIMITER);
+            String property = orderParams[ORDER_EXPRESSION_PROPERTY_INDEX].trim();
             String direction = "";
 
-            if (parts.length > 1) {
-                direction = parts[1].trim();
+            if (orderParams.length > ORDER_EXPRESSION_DIRECTION_INDEX) {
+                direction = orderParams[ORDER_EXPRESSION_DIRECTION_INDEX].trim();
             }
 
             Sort.Order order = direction.equalsIgnoreCase("desc") ?
-                    Sort.Order.desc(field) : Sort.Order.asc(field);
+                    Sort.Order.desc(property) : Sort.Order.asc(property);
 
             orders.add(order);
         }
@@ -42,17 +50,17 @@ public class SortUtility {
     }
 
     private static Sort getDefaultSort() {
-        return Sort.by(Sort.Order.asc("id"));
+        return DEFAULT_SORT;
     }
 
     private static boolean isValidSortExpression(String sortExpression) {
 
         if (sortExpression != null && !sortExpression.isBlank()) {
 
-            String[] sortFields = sortExpression.split(",");
+            String[] orderExpressions = sortExpression.split(SORT_EXPRESSION_DELIMITER);
 
-            for (String sortField: sortFields) {
-                if(!Pattern.matches(SORT_FIELD_REGEX, sortField)) {
+            for (String orderExpression: orderExpressions) {
+                if(!Pattern.matches(ORDER_EXPRESSION_REGEX, orderExpression)) {
                     return false;
                 }
             }
