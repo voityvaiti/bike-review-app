a5136c26cbee2125dfc4960f1e59fa89269123df
voityvaiti <flashlight@i.ua>
2023-12-15 21:35:38 +0200

    Method in Motorcycle repo and service for simple search by query

--- src/main/java/com/myproject/bikereviewapp/repository/MotorcycleRepository.java
+++ src/main/java/com/myproject/bikereviewapp/repository/MotorcycleRepository.java
@@ -1,10 +1,15 @@
 package com.myproject.bikereviewapp.repository;
 
 import com.myproject.bikereviewapp.entity.Motorcycle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface MotorcycleRepository extends JpaRepository<Motorcycle, Long> {
+
+    Page<Motorcycle> getAllByModelContainingIgnoreCaseOrBrandNameContainingIgnoreCase(String model, String brandName, Pageable pageable);
+
 }
