0862d0f0634eb8470e0394d96dc926d338136ec0
voityvaiti <flashlight@i.ua>
2024-01-06 15:24:41 +0200

    UserControllerTest new test methods

--- src/test/java/com/myproject/bikereviewapp/controller/UserControllerTest.java
+++ src/test/java/com/myproject/bikereviewapp/controller/UserControllerTest.java
@@ -1,9 +1,7 @@
 
 package com.myproject.bikereviewapp.controller;
 
-import com.myproject.bikereviewapp.entity.Review;
-import com.myproject.bikereviewapp.entity.Role;
-import com.myproject.bikereviewapp.entity.User;
+import com.myproject.bikereviewapp.entity.*;
 import com.myproject.bikereviewapp.service.abstraction.ReviewService;
 import com.myproject.bikereviewapp.service.abstraction.UserService;
 import com.myproject.bikereviewapp.utility.SortUtility;
@@ -15,14 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -67,7 +64,7 @@ class UserControllerTest {
     private static String sortStr;
     private static BindingResult mockBindingResult;
     private static Model mockModel;
-
+    private static Authentication mockAuthentication;
 
 
     @BeforeAll
@@ -78,9 +75,10 @@ class UserControllerTest {
                 new User(3L, "username3", "password3", true, Role.CLIENT, "publicName3")
         ));
 
+        Motorcycle sampleMotorcycle = new Motorcycle(1L, "someModel", new Brand(1L, "someName", "someCountry"));
         reviewPage = new PageImpl<>(List.of(
-                new Review(),
-                new Review()
+                new Review(1L, "bodyContent", LocalDate.of(2000, 1, 10), (short) 3, sampleMotorcycle, user),
+                new Review(2L, "bodyContent", LocalDate.of(2000, 2, 20), (short) 4, sampleMotorcycle, user)
         ));
 
         user = new User(8L, USERNAME, "somePassword", true, Role.CLIENT, "somePublicName");
@@ -91,13 +89,18 @@ class UserControllerTest {
         sortStr = "id:asc";
         mockBindingResult = mock(BindingResult.class);
         mockModel = mock(Model.class);
+
+        mockAuthentication = mock(Authentication.class);
+        when(mockAuthentication.getName()).thenReturn(USERNAME);
     }
 
     @BeforeEach
     void setUp() {
-        when(sortUtility.parseSort(anyString())).thenReturn(sort);
         when(userService.getAll(any(PageRequest.class))).thenReturn(userPage);
+        when(userService.getByUsername(anyString())).thenReturn(user);
+        when(reviewService.getReviewsByUserId(anyLong(), any(Pageable.class))).thenReturn(reviewPage);
 
+        when(sortUtility.parseSort(anyString())).thenReturn(sort);
         when(mockBindingResult.hasErrors()).thenReturn(false);
     }
 
@@ -152,6 +155,94 @@ class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("currentPageNumber"))
                 .andExpect(model().attributeExists("currentSort"));
+    }
+
+    @Test
+    void showCurrentUserProfile_shouldReturnAppropriateView_ifUserIsAuthenticated() throws Exception {
+
+        mockMvc.perform(get("/users/profile")
+                        .principal(mockAuthentication))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user/profile"));
+    }
+
+    @Test
+    void showCurrentUserProfile_shouldAddUserModelAttribute() throws Exception {
+
+        mockMvc.perform(get("/users/profile")
+                        .principal(mockAuthentication))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("user", user));
+    }
+
+    @Test
+    void showCurrentUserProfile_shouldAddReviewPageModelAttribute() throws Exception {
+
+        mockMvc.perform(get("/users/profile")
+                        .principal(mockAuthentication))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("reviewPage", reviewPage));
+    }
+
+    @Test
+    void showCurrentUserProfile_shouldShouldGetUserByProperUsername() throws Exception {
+
+        when(userService.getByUsername(USERNAME)).thenReturn(user);
+
+        mockMvc.perform(get("/users/profile")
+                        .principal(mockAuthentication))
+                .andExpect(status().isOk());
+
+        verify(userService).getByUsername(USERNAME);
+    }
+
+    @Test
+    void showCurrentUserProfile_shouldMakeReviewPageRequestByProperParams_whenRequestContainAppropriateParams() throws Exception {
+
+        when(sortUtility.parseSort(sortStr)).thenReturn(sort);
+
+        mockMvc.perform(get("/users/profile")
+                        .param("reviewPageNumber", String.valueOf(pageNumber))
+                        .param("reviewPageSize", String.valueOf(pageSize))
+                        .param("reviewSort", sortStr)
+                        .principal(mockAuthentication))
+                .andExpect(status().isOk());
+
+        verify(sortUtility).parseSort(sortStr);
+        verify(reviewService).getReviewsByUserId(user.getId(), PageRequest.of(pageNumber, pageSize, sort));
+    }
+
+    @Test
+    void showCurrentUserProfile_shouldAddReviewPageNumberAndSortModelAttributes_whenRequestContainAppropriateParams() throws Exception {
+
+        mockMvc.perform(get("/users/profile")
+                        .param("reviewPageNumber", String.valueOf(pageNumber))
+                        .param("reviewPageSize", String.valueOf(pageSize))
+                        .param("reviewSort", sortStr)
+                        .principal(mockAuthentication))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("currentReviewPageNumber", pageNumber))
+                .andExpect(model().attribute("currentReviewSort", sortStr));
+    }
+
+    @Test
+    void showCurrentUserProfile_shouldAddDefaultReviewPageNumberAndSortModelAttributes_whenRequestDoesNotContainAppropriateParams() throws Exception {
+
+        mockMvc.perform(get("/users/profile")
+                        .principal(mockAuthentication))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("currentReviewPageNumber"))
+                .andExpect(model().attributeExists("currentReviewSort"));
+    }
+
+    @Test
+    void showCurrentUserProfile_shouldForbidRequest_whenUserIsNotAuthenticated() throws Exception {
+
+        mockMvc.perform(get("/users/profile"))
+                .andExpect(status().isUnauthorized());
+
+        verify(userService, never()).getByUsername(anyString());
+        verify(reviewService, never()).getReviewsByUserId(anyLong(), any(PageRequest.class));
     }
 
     @Test
