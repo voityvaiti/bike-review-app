8a2ab92ada4c62b256256b171f28203f1c0445fa
voityvaiti <flashlight@i.ua>
2024-01-31 02:15:00 +0200

    UserDetailsServiceImpl adapted to UserPrincipal and moved to security package

--- src/main/java/com/myproject/bikereviewapp/security/UserDetailsServiceImpl.java
+++ src/main/java/com/myproject/bikereviewapp/security/UserDetailsServiceImpl.java
@@ -1,15 +1,13 @@
-package com.myproject.bikereviewapp.service.impl;
+package com.myproject.bikereviewapp.security;
 
 import com.myproject.bikereviewapp.entity.User;
 import com.myproject.bikereviewapp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,13 +21,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         User user = userRepository.findByUsername(username).orElseThrow(
-                () -> new UsernameNotFoundException("User with not found with username: " + username)
+                () -> new UsernameNotFoundException("User not found with username: " + username)
         );
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                List.of(new SimpleGrantedAuthority(user.getRole().name()))
-        );
+        return new UserPrincipal(user);
     }
 }
