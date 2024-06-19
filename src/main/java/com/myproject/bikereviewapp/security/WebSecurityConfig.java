b7edb753d59a93549bcb88b311a956f1d2cc2f60
voityvaiti <flashlight@i.ua>
2024-06-19 11:52:10 +0300

    CSRF disabled

--- src/main/java/com/myproject/bikereviewapp/security/WebSecurityConfig.java
+++ src/main/java/com/myproject/bikereviewapp/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -45,7 +46,8 @@ public class WebSecurityConfig {
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/")
-                )
+
+                ).csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 
