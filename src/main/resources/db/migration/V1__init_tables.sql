91d1963ed2a16429c83f9eb94240866150c364ac
voityvaiti <flashlight@i.ua>
2024-07-10 23:30:55 +0300

    Spacing in migration files

--- src/main/resources/db/migration/V1__init_tables.sql
+++ src/main/resources/db/migration/V1__init_tables.sql
@@ -7,31 +7,31 @@ CREATE TABLE IF NOT EXISTS brand
 
 CREATE TABLE IF NOT EXISTS motorcycle
 (
-    id SERIAL PRIMARY KEY,
+    id       SERIAL PRIMARY KEY,
     brand_id INT NOT NULL,
-    model VARCHAR(50),
-    FOREIGN KEY (brand_id) REFERENCES brand(id) ON UPDATE CASCADE ON DELETE RESTRICT
+    model    VARCHAR(50),
+    FOREIGN KEY (brand_id) REFERENCES brand (id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS usr
 (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(20) NOT NULL UNIQUE,
-    password CHAR(60) NOT NULL,
-    enabled BOOLEAN NOT NULL,
-    role VARCHAR(20) NOT NULL,
+    id          SERIAL PRIMARY KEY,
+    username    VARCHAR(20) NOT NULL UNIQUE,
+    password    CHAR(60)    NOT NULL,
+    enabled     BOOLEAN     NOT NULL,
+    role        VARCHAR(20) NOT NULL,
     public_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS review
 (
-    id BIGSERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    motorcycle_id INT NOT NULL,
-    body VARCHAR(1000) NOT NULL,
-    rating SMALLINT NOT NULL,
-    publication_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES usr(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (motorcycle_id) REFERENCES motorcycle(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    id               BIGSERIAL PRIMARY KEY,
+    user_id          INT           NOT NULL,
+    motorcycle_id    INT           NOT NULL,
+    body             VARCHAR(1000) NOT NULL,
+    rating           SMALLINT      NOT NULL,
+    publication_date DATE          NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES usr (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (motorcycle_id) REFERENCES motorcycle (id) ON UPDATE CASCADE ON DELETE CASCADE,
     UNIQUE (user_id, motorcycle_id)
 );
--- src/main/resources/db/migration/V4__reaction_table.sql
+++ src/main/resources/db/migration/V4__reaction_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS reaction
 (
-    id BIGSERIAL PRIMARY KEY,
-    review_id BIGINT NOT NULL,
-    user_id INT NOT NULL,
-    is_like BOOLEAN NOT NULL,
-    FOREIGN KEY (review_id) REFERENCES review(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES usr(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    id        BIGSERIAL PRIMARY KEY,
+    review_id BIGINT  NOT NULL,
+    user_id   INT     NOT NULL,
+    is_like   BOOLEAN NOT NULL,
+    FOREIGN KEY (review_id) REFERENCES review (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES usr (id) ON UPDATE CASCADE ON DELETE CASCADE,
     UNIQUE (review_id, user_id)
 );
