69f0cbe6644f1a7445d6bf92a8013d4855aea413
voityvaiti <flashlight@i.ua>
2024-07-14 23:41:22 +0300

    FileUploadPojo created to return data after uploading file to the cloud

---
+++ src/main/java/com/myproject/bikereviewapp/entity/pojo/FileUploadPojo.java
@@ -0,0 +1,16 @@
+package com.myproject.bikereviewapp.entity.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileUploadPojo {
+
+    private String name;
+
+    private String url;
+
+}
