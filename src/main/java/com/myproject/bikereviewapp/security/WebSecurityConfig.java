4ff86be7bd8686967aec3ccfb336c882f13c38e4
voityvaiti <flashlight@i.ua>
2024-08-23 15:13:20 +0300

    Cache config for app static resources

--- src/main/java/com/myproject/bikereviewapp/security/WebSecurityConfig.java
+++ src/main/java/com/myproject/bikereviewapp/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.myproject.bikereviewapp.security;
 
 import com.myproject.bikereviewapp.entity.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,17 +12,22 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
 
     private static final String ADMIN = Role.ADMIN.name();
     private static final String STUFF = Role.STUFF.name();
 
+    @Value("${cache.control.static.max-age}")
+    private int cacheControlMaxAge;
 
 
     @Bean
@@ -37,7 +43,7 @@ public class WebSecurityConfig {
 
                         .requestMatchers(antMatcher("/reviews/**")).authenticated()
                         .requestMatchers(antMatcher("/users/profile/**")).authenticated()
-                
+
                         .anyRequest().permitAll())
 
                 .formLogin(form -> form
@@ -49,6 +55,16 @@ public class WebSecurityConfig {
 
                 ).csrf(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(cacheControlMaxAge)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
     }
 
     @Bean
