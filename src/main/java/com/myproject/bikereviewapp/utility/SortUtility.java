46ffc94eb5d413d78389e8142dfd1be858ca09ee
voityvaiti <flashlight@i.ua>
2024-09-01 20:28:41 +0300

    SortUtility: validation of sort properties and other refactoring

--- src/main/java/com/myproject/bikereviewapp/utility/SortUtility.java
+++ src/main/java/com/myproject/bikereviewapp/utility/SortUtility.java
@@ -1,14 +1,24 @@
 package com.myproject.bikereviewapp.utility;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.ManagedType;
+import jakarta.persistence.metamodel.Metamodel;
+import jakarta.persistence.metamodel.SingularAttribute;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class SortUtility {
+
+    private final EntityManager entityManager;
 
     private static final String ORDER_EXPRESSION_REGEX = "^\\s*[A-Za-z.]+\\s*(:\\s*((asc|desc)|)\\s*)?$";
 
@@ -21,51 +31,85 @@ public class SortUtility {
     private static final Sort DEFAULT_SORT = Sort.by(Sort.Order.asc("id"));
 
 
-    public Sort parseSort(String sortExpression) {
+    public Sort parseSort(String sortExpression, Class<?> entityClass) {
+
+        validateEntityClass(entityClass);
 
         if (!isValidSortExpression(sortExpression)) {
             return getDefaultSort();
         }
 
-        List<Sort.Order> orders = new ArrayList<>();
+        List<Sort.Order> orders = Arrays.stream(sortExpression.split(SORT_EXPRESSION_DELIMITER))
+                .map(expr -> parseOrderExpression(expr, entityClass))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
 
-        String[] orderExpressions = sortExpression.split(SORT_EXPRESSION_DELIMITER);
+        return orders.isEmpty() ? getDefaultSort() : Sort.by(orders);
+    }
 
-        for (String orderExpression : orderExpressions) {
-            String[] orderParams = orderExpression.split(ORDER_EXPRESSION_DELIMITER);
-            String property = orderParams[ORDER_EXPRESSION_PROPERTY_INDEX].trim();
-            String direction = "";
+    private Sort.Order parseOrderExpression(String orderExpression, Class<?> entityClass) {
 
-            if (orderParams.length > ORDER_EXPRESSION_DIRECTION_INDEX) {
-                direction = orderParams[ORDER_EXPRESSION_DIRECTION_INDEX].trim();
+        String[] orderParams = orderExpression.split(ORDER_EXPRESSION_DELIMITER);
+
+        String property = orderParams[ORDER_EXPRESSION_PROPERTY_INDEX].trim();
+        String direction = orderParams.length > ORDER_EXPRESSION_DIRECTION_INDEX ?
+                orderParams[ORDER_EXPRESSION_DIRECTION_INDEX].trim() : "";
+
+        if (isValidProperty(property, entityClass)) {
+
+            return direction.equalsIgnoreCase("desc") ? Sort.Order.desc(property) : Sort.Order.asc(property);
+        } else {
+            return null;
+        }
+    }
+
+    private static boolean isValidSortExpression(String sortExpression) {
+        return Optional.ofNullable(sortExpression)
+                .map(expression -> !expression.isBlank())
+                .orElse(false) && Arrays.stream(sortExpression.split(SORT_EXPRESSION_DELIMITER))
+                .allMatch(orderExpression -> Pattern.matches(ORDER_EXPRESSION_REGEX, orderExpression));
+    }
+
+    public boolean isValidProperty(String propertyPath, Class<?> entityClass) {
+
+        Metamodel metamodel = entityManager.getMetamodel();
+        EntityType<?> entityType = metamodel.entity(entityClass);
+
+        String[] properties = propertyPath.split("\\.");
+
+        ManagedType<?> currentType = entityType;
+
+        for (int i = 0; i < properties.length; i++) {
+            String property = properties[i];
+            SingularAttribute<?, ?> attribute;
+
+            try {
+                attribute = currentType.getSingularAttribute(property);
+            } catch (IllegalArgumentException e) {
+                return false;
             }
 
-            Sort.Order order = direction.equalsIgnoreCase("desc") ?
-                    Sort.Order.desc(property) : Sort.Order.asc(property);
+            if (i < properties.length - 1) {
 
-            orders.add(order);
+                if (attribute.getType() instanceof ManagedType) {
+                    currentType = (ManagedType<?>) attribute.getType();
+
+                } else {
+                    return false;
+                }
+            }
         }
-
-        return Sort.by(orders);
+        return true;
     }
 
     public static Sort getDefaultSort() {
         return DEFAULT_SORT;
     }
 
-    private static boolean isValidSortExpression(String sortExpression) {
-
-        if (sortExpression != null && !sortExpression.isBlank()) {
-
-            String[] orderExpressions = sortExpression.split(SORT_EXPRESSION_DELIMITER);
-
-            for (String orderExpression: orderExpressions) {
-                if(!Pattern.matches(ORDER_EXPRESSION_REGEX, orderExpression)) {
-                    return false;
-                }
-            }
-            return true;
+    private void validateEntityClass(Class<?> entityClass) {
+        if (!entityClass.isAnnotationPresent(Entity.class)) {
+            throw new IllegalArgumentException("Provided class is not a JPA entity: " + entityClass.getName());
         }
-        return false;
     }
+
 }
